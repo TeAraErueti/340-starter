0be4e30abb3e63b30b1f108c37519d9fea39c616
TeAra <keitepai1@gmail.com>
2025-05-21 20:16:57 +1200

    update

--- controllers/invController.js
+++ controllers/invController.js
@@ -1,22 +1,22 @@
 const invModel = require("../models/inventory-model")
-const utilities = require("../utilities")
+const utilities = require("../utilities/")
 
 const invCont = {}
 
 /* ***************************
- *Build inventory by classfication view
+ *  Build inventory by classification view
  * ************************** */
 invCont.buildByClassificationId = async function (req, res, next) {
   const classification_id = req.params.classificationId
-  const data = invModel.getInventoryByClassificationId(classification_id)
+  const data = await invModel.getInventoryByClassificationId(classification_id)
   const grid = await utilities.buildClassificationGrid(data)
   let nav = await utilities.getNav()
   const className = data[0].classification_name
   res.render("./inventory/classification", {
-    title: className + "vehicles",
+    title: className + " vehicles",
     nav,
     grid,
- })
+  })
 }
 
 module.exports = invCont
